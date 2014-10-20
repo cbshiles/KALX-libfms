ff635c18bbc4c40db778d951b1d977104b7621a4
jl2372 <jl2372@cornell.edu>
2014-10-20 15:17:29 -0400

    "first commit"

--- xllfms/xllpwflat.cpp
+++ xllfms/xllpwflat.cpp
@@ -14,6 +14,7 @@ static AddInX xai_pwflat_value(
 	.Category(CATEGORY)
 	.FunctionHelp(_T("Evaluate a piecewise flat curve at Time."))
 );
+
 double WINAPI xll_pwflat_value(double t, xfp* pt, xfp* pf, double f_)
 {
 #pragma XLLEXPORT
@@ -33,4 +34,33 @@ double WINAPI xll_pwflat_value(double t, xfp* pt, xfp* pf, double f_)
 
 // static AddInX xai_pwflat_integral( // zoewangforest
 // static AddInX xai_pwflat_spot( // zzx5008 
-// static AddInX xai_pwflat_discount(
+
+
+static AddInX xai_pwflat_discount( //jl2372
+	FunctionX(XLL_DOUBLEX, _T("?xll_pwflat_discount"), _T("PWFLAT.DISCOUNT"))
+	.Arg(XLL_DOUBLEX, _T("Time"), _T("is time at which the discount of the piecewise constant curve is to be evaluated"))
+	.Arg(XLL_FPX, _T("Times"), _T("is an array of times"))
+	.Arg(XLL_FPX, _T("Forwards"), _T("is an array of forwards"))
+	.Arg(XLL_DOUBLEX, _T("_Missing Value"), _T("is an optional value used to undefined values. Default is 0"))
+	.Category(CATEGORY)
+	.FunctionHelp(_T("Evaluates the discount value of a piecewise flat curve at Time."))
+	);
+double WINAPI xll_pwflat_discount(double t, xfp* pt, xfp* pf, double f_)
+{
+#pragma XLLEXPORT
+	double d{ std::numeric_limits<double>::quiet_NaN() };
+
+	try {
+		ensure(size(*pt) == size(*pt));
+
+		d = discount(t, size(*pt), pt->array, pf->array, f_);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return d;
+}
+
+
+
