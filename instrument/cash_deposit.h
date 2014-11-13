a6c1181b574f77819753f1b5f07170e7f3e62c04
dgtsx <dgtsx@hotmail.com>
2014-11-12 20:06:37 -0500

    Update

--- instrument/cash_deposit.h
+++ instrument/cash_deposit.h
@@ -7,6 +7,55 @@ namespace fms {
 namespace fixed_income {
 
 	//!!!dgtsx inherit from fixed_income::interest_rate_leg
+
+	template<class T = double, class C = double>
+	class cash_deposit : public fixed_income::leg<T, C> {
+	public:
+		
+		// typical cash deposit conventions
+		cash_deposit()
+			:
+			leg(1, fms::datetime::UNIT_DAYS, FREQ_SEMIANNUALLY, fms::datetime::DCB_ACTUAL_360, fms::datetime::ROLL_MODIFIED_FOLLOWING, fms::datetime::CALENDAR_NONE)
+		{ }
+		cash_deposit(
+			int count, fms::datetime::time_unit unit,
+			fms::datetime::day_count_basis dcb = DCB_30U_360,
+			fms::datetime::roll_convention roll = ROLL_MODIFIED_FOLLOWING,
+			fms::datetime::holiday_calendar cal = CALENDAR_NONE)
+			:
+			leg(count, unit, FREQ_SEMIANNUALLY, dcb, roll, cal)
+		{ }
+
+		cash_deposit(const cash_deposit&) = default;
+		cash_deposit& operator=(const cash_deposit&) = default;
+		~cash_deposit()
+		{ }
+
+		cash_deposit<T, C>& fix(const fms::datetime::date& set, const fms::datetime::date& eff, const C& rate)
+		{
+			datetime::date d0(eff);
+			add(d0.diffyears(set), -1);
+
+			datetime::date d1(d0);
+			d1.incr(count_, unit_).adjust(roll_, cal_);
+			add(d1.diffyears(set), 1 + rate*d1.diff_dcb(d0, dcb_));
+
+			ensure(cash(1) > 0 || !"fms::fixed_income::cash_deposit: coupon not arbitrage free");
+
+			return *this;
+		}
+		// settle using T + days
+		cash_deposit<T, C>& fix(const fms::datetime::date& set, int days, const C& rate)
+		{
+			fms::datetime::date eff(set);
+
+			eff.incr(days, fms::datetime::UNIT_DAYS).adjust(roll_, call_);
+
+			return fix(set, eff, rate);
+		}
+	};
+
+	/*
 	template<class T = double, class C = double>
 	class cash_deposit : public vector::instrument<T,C,fms::datetime::date> {
 	public:
@@ -61,6 +110,7 @@ namespace fixed_income {
 			return fix(set, eff, rate);
 		}
 	};
+	*/
 
 } // namespace fixed_income
 } // namespace fms
