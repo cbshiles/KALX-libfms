f5107cbb96a09fe875024166cdfd9943039bf6fc
keithalewis <kal@kalx.net>
2014-12-08 18:54:22 -0500

    Implement bms model

--- option/bms.h
+++ option/bms.h
@@ -12,9 +12,22 @@ namespace option {
 		inline auto value(R r, F s, S sigma, K k, T t, K* df = 0, K* ddf = 0, K* ds = 0, K* dt = 0)
 			-> decltype(r + s + sigma + k + t)
 		{
-			// implement functions from final exam part 1 here!!!
+			K df_ = 0, ddf_ = 0, ds_ = 0, dt_ = 0;
+			K ert = exp(r*t);
+			F f = s*ert;
 
-			return 0;
+			K v = fms::option::black::value(f, sigma, k, t, &df_, &ddf_, &ds_, &dt_);
+		
+			if (df)
+				*df += df_;
+			if (ddf)
+				*ddf += ert*ddf_;
+			if (ds)
+				*ds += ds_/ert;
+			if (dt)
+				*dt += (dt_ - r*v)/ert;
+
+			return v/ert;
 		}
 
 	}
